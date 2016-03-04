059c66e04748690959e1abc32df7c7627f65770b
nivesh singh <nivesh2@gmail.com>
2016-03-04 05:58:50 +0000

    middleware added

---
+++ middleware/base.middleware.js
@@ -0,0 +1,30 @@
+'use strict';
+/**
+ * Adding various middleware to app
+ */
+
+module.exports = function(app){
+    const bodyParser = require('body-parser'),
+          logger = require('morgan'),
+          cors = require('cors');
+    
+    //Access-Control-Allow-Origin
+    app.use(cors());
+    
+    //throws 400 error to next, if JSON is not valid 
+    app.use(bodyParser.json({
+        strict:true,
+        })); 
+        
+    //parses the url encoded strings    
+    app.use(bodyParser.urlencoded({
+        extended:true,
+        }));
+        
+        
+    
+   
+    //logs incoming request in dev pattern 
+    app.use(logger('dev'));
+};
+
